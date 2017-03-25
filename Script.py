d6befc819735c5caff9bedb82e6a9faf79134b7b
lexpapaioannou <papaioannou.alexander1@gmail.com>
2017-03-25 16:00:59 -0500

    Add files via upload

--- Script.py
+++ Script.py
@@ -1,3 +1,8 @@
+#This is a test script to keep the computer from loggin me out.
+#LUC computers log off after 10 min of inactivity.
+#I need to render something that could potentially take over night.
+#This script, succesful so far, is to make sure the computer continues rendering without me having to stay up all night just to make sure the computer doesn't log me out or delete all my work.
+
 import time, os
 
 while True:
